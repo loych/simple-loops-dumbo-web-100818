7f506f3d67f8c15f9ba5c3f4d426326895215484
IS Choi <insil321@hotmail.com>
2018-09-27 13:00:02 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -10,6 +10,7 @@ def loop_iterator(number_of_times)
   #  phrase and break when the counter hits the appropriate number...
   7.times do
   phrase = "Welcome to Flatiron School's Web Development Course!"
+  puts phrase
   
 end
 
